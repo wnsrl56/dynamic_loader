43a71bd7bdaa10604a47f803ba31c4b75b9ea65d
Jun <wnsrl56@gmail.com>
2018-08-01 19:25:34 +0900

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <dynamic-link/>
+  </div>
+</template>
+
+<script>
+import DynamicLink from './components/DynamicLink';
+
+export default {
+  name: 'App',
+  components: {
+    DynamicLink
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/components/DynamicLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="component"/>
+</template>
+
+<script>
+export default {
+  name: 'DynamicLink',
+  data () {
+    return {
+      component: null,
+    }
+  },
+  computed: {
+    loader() {
+      return () => import('@/components/HelloWorld');
+    },
+  },
+  mounted() {
+    this.loader()
+      .then(() => {
+        this.component = () => this.loader();
+      })
+      .catch(() => {
+      });
+  },
+}
+</script>
+<style scoped>
+</style>
